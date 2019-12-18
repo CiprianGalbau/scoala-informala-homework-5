2f1bb3a0544c8067f1696750021765bfe832d41d
Ciprian <cipriangalbau@gmail.com>
2019-12-18 19:49:13 +0200

    Commit readme file

--- IntroToJava/src/ro/sci/intro/Main.java
+++ IntroToJava/src/ro/sci/intro/Main.java
@@ -3,9 +3,7 @@ package ro.sci.intro;
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello from my first branch");
-
+        System.out.println("Hello world!");
     }
 
 }
